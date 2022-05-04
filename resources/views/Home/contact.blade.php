ae5deb4d092f5a4cb2d184b8c4aec77c91957d55
AtaberkMercan <ataberkmercan21@gmail.com>
2022-05-04 14:00:46 +0300

    Added Contact US.

---
+++ resources/views/Home/contact.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.frontbase')
+@section('title','Contact Us | '.$setting->title)
+@section('desc',$setting->desc)
+@section('keys',$setting->keys)
+@section('icon',\Illuminate\Support\Facades\Storage::url($setting->icon))
+@section('sidebar')
+    @include('home.sidebar')
+@endsection
+@section('content')
+    <!-- Page Header Start -->
+    <div class="container-fluid bg-secondary mb-5">
+        <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 300px">
+            <h1 class="font-weight-semi-bold text-uppercase mb-3">Contact Us</h1>
+            <div class="d-inline-flex">
+                <p class="m-0"><a href="{{route('home')}}">Home</a></p>
+                <p class="m-0 px-2">-</p>
+                <p class="m-0">Contact Us</p>
+            </div>
+        </div>
+    </div>
+    <!-- Page Header End -->
+    <!-- Contact Start -->
+    <div class="container-fluid pt-5">
+        <div class="text-center mb-4">
+            <h2 class="section-title px-5"><span class="px-2">Contact For Any Queries</span></h2>
+        </div>
+        <div class="row px-xl-5">
+            <div class="col-lg-7 mb-5">
+                <div class="contact-form">
+                    <div id="success"></div>
+                    <form name="sentMessage" id="contactForm" novalidate="novalidate">
+                        <div class="control-group">
+                            <input type="text" class="form-control" id="name" placeholder="Your Name"
+                                   required="required" data-validation-required-message="Please enter your name" />
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <div class="control-group">
+                            <input type="email" class="form-control" id="email" placeholder="Your Email"
+                                   required="required" data-validation-required-message="Please enter your email" />
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <div class="control-group">
+                            <input type="text" class="form-control" id="subject" placeholder="Subject"
+                                   required="required" data-validation-required-message="Please enter a subject" />
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <div class="control-group">
+                            <textarea class="form-control" rows="6" id="message" placeholder="Message"
+                                      required="required"
+                                      data-validation-required-message="Please enter your message"></textarea>
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <div>
+                            <button class="btn btn-primary py-2 px-4" type="submit" id="sendMessageButton">Send
+                                Message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-5 mb-5">
+                <h5 class="font-weight-semi-bold mb-3">Get In Touch</h5>
+                <p>{!!$setting->contact!!}</p>
+
+            </div>
+        </div>
+    </div>
+    <!-- Contact End -->
+@endsection
