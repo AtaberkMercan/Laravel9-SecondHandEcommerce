6c6dbcbc46ed68acb057f4d61a817577f9f162df
AtaberkMercan <ataberkmercan21@gmail.com>
2022-05-18 19:25:16 +0300

    Added admin user Permission for Admin Panel on Middleware.

--- routes/web.php
+++ routes/web.php
@@ -31,10 +31,10 @@ use Illuminate\Support\Facades\Route;
     Route::post('/storemessage',[HomeController::class,'storemessage'])->name('storemessage');
     Route::get('/faq',[HomeController::class,'faq'])->name('faq');
     Route::post('/storecomment',[HomeController::class,'storecomment'])->name('storecomment');
-    Route::view('/loginuser','home.login');
-    Route::view('/registeruser','home.register');
+    Route::view('/loginuser','home.login')->name('loginuser');
+    Route::view('/registeruser','home.register')->name('registeruser');
     Route::get('/logoutuser',[HomeController::class,'logout'])->name('logoutuser');
-    Route::view('/loginadmin','admin.login');
+    Route::view('/loginadmin','admin.login')->name('loginadmin');
     Route::post('/loginadmincheck',[HomeController::class,'loginadmincheck'])->name('loginadmincheck');
 
 //*************************************************************************//
@@ -46,8 +46,9 @@ use Illuminate\Support\Facades\Route;
     // TEST//
     Route::get('/test',[HomeController::class,'test']);
     //************************************** ADMIN PANEL ROUTES ***************//
-    Route::prefix('admin')->name('admin.')->group(function (){
+    Route::middleware('admin')->prefix('admin')->name('admin.')->group(function (){
         Route::get('/',[AdminHomeController::class,'index'])->name('index');
+
         //************************************** GENERAL ROUTES  ***************//
         Route::get('/setting',[AdminHomeController::class,'setting'])->name('setting');
         Route::post('/setting',[AdminHomeController::class,'settingupdate'])->name('setting.update');
